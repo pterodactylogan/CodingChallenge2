b524bdfc16a30d55af81e1cc272f81563b053284
Mégane <megane@MacBook-Air-de-Megane-2.local>
2018-02-13 11:34:01 -0500

    Recursive print out added

--- recursiveSplicing.py
+++ recursiveSplicing.py
@@ -10,8 +10,12 @@ accepted = ['A','C','G','T']
 def recursive_wrapper(gene1, gene2):
     '''wrapper for genesplicer that cuts strings down to matching lengths'''
     #if one is already a substring of the other--DONE
-    if gene1 in gene2: return gene2
-    if gene2 in gene1: return gene1
+    if gene1 in gene2: 
+    	print_out(gene2, gene1, gene2)
+    	return gene2
+    if gene2 in gene1: 
+    	print_out(gene1, gene1, gene2)
+    	return gene1
     
     #clean up strings so they are same length
     if len(gene1)==len(gene2): 
@@ -24,8 +28,12 @@ def recursive_wrapper(gene1, gene2):
         forward=recursive_splicing(gene1,gene2[:len(gene1)])+gene2[len(gene1):]
         reverse=gene2[:-len(gene1)]+recursive_splicing(gene2[-len(gene1):],gene1)
         
-    if len(forward)<len(reverse): return forward
-    else: return reverse
+    if len(forward)<len(reverse): 
+    	print_out(forward, gene1, gene2)
+    	return forward
+    else: 
+    	print_out(reverse, gene1, gene2)
+    	return reverse
     
 def recursive_splicing(gene1, gene2):
     '''takes two strings of the same length and returns the splicing'''
@@ -38,8 +46,8 @@ def recursive_splicing(gene1, gene2):
         
         
 def main():
-    gene1="CGA"
-    gene2="GCTGTACGA"
+    gene1 = input("Input the first gene: ")      
+    gene2 = input("Input the second gene: ")
     check_string(gene1)
     check_string(gene2)
     print(recursive_wrapper(gene1,gene2))
@@ -49,5 +57,13 @@ def check_string(str1):
     for char in str1:
         if not char in accepted:
             raise Exception("invalid input")
+            
+def print_out(shorter, str1, str2):   
+	'''Function printing out the results'''
+	print ("********************************************")
+	print ("string 1 - "+str1+"    string 2 - "+str2)
+	print("shortest string that has both as substring")
+	print(shorter + "\t(length "+str(len(shorter))+")")
+	print ("********************************************")  
     
 main()
